5c1d70b61d6b61c9158233d7a4fcda0ca2b6fcdf
lyritzis286 <lyritzis286@gmail.com>
2026-07-13 19:45:59 +0300

    Initial Commit

---
+++ src/main/java/gr/aueb/cf/starterpro/StarterproApplication.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.starterpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/aueb/cf/starterpro/StarterproApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.starterpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
