f663a6bf8ce1656a4d554719dc6b15abed897e1b
lyritzis286 <lyritzis286@gmail.com>
2026-07-13 20:41:31 +0300

    Add blank lines for improved readability in index.html and StarterproApplication.java

--- src/main/java/gr/aueb/cf/starterpro/StarterproApplication.java
+++ src/main/java/gr/aueb/cf/starterpro/StarterproApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StarterproApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(StarterproApplication.class, args);
 	}
 
